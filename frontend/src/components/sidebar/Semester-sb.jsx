9f977fa7eaba6ecea8e6a7bcc2e69063995877eb
Georgios Drivas <78447254+GeorgiosDrivas@users.noreply.github.com>
2024-03-24 18:17:00 +0200

    Update Semester-sb.jsx

--- frontend/src/components/sidebar/Semester-sb.jsx
+++ frontend/src/components/sidebar/Semester-sb.jsx
@@ -1,7 +1,19 @@
-export default function Semester(){
-    return(
+import Summary from "../dashboard/Summary";
+import Semester from "../dashboard/Courses";
+
+export default function SemesterSb({ data }) {
+    return (
         <>
             <h1>Semester</h1>
+
+            <section className="dashboard-section position-relative">
+                <Summary user={data} />
+            </section>
+            <section className="dashboard-section position-relative">
+                <div className="container">
+                    <Semester user={data} />
+                </div>
+            </section>
         </>
     )
 }
