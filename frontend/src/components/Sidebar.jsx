45d1e4bb6da7f27f1f669e5852a8f8d7851e11dd
GiorgosDrivas <drivasgiorgos9@gmail.com>
2024-03-25 01:53:56 +0200

    Sidebar

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,18 +1,22 @@
 import { Link } from 'react-router-dom';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import AutoStoriesIcon from '@mui/icons-material/AutoStories';
+import EventIcon from '@mui/icons-material/Event';
+import NewspaperIcon from '@mui/icons-material/Newspaper';
+import SettingsIcon from '@mui/icons-material/Settings';
+import LogoutIcon from '@mui/icons-material/Logout';
 
 export default function Sidebar() {
     return (
         <>
-            <section className="right-sidebar position-relative w-100">
-                <ul id="sidebar-ul" className="position-fixed">
-                    <li><Link to="/">Dashboard</Link></li>
-                    <li><Link to="/semester">Semester</Link></li>
-                    <li><Link to="/events">Events</Link></li>
-                    <li><Link to="/news">News</Link></li>
-                    <li><Link to="/settings">Settings</Link></li>
-                    <li><Link to="/logout">Logout</Link></li>
-                </ul>
-            </section>
+            <ul id="sidebar-ul" className="right-sidebar position-fixed">
+                <li><Link to="/"><DashboardIcon style={{ fill: '#f9fafc', width: "35px", height: "35px", marginRight: "25px" }} />Dashboard</Link></li>
+                <li><Link to="/semester"><AutoStoriesIcon style={{ fill: '#f9fafc', width: "35px", height: "35px", marginRight: "25px" }} />Semester</Link></li>
+                <li><Link to="/events"><EventIcon style={{ fill: '#f9fafc', width: "35px", height: "35px", marginRight: "25px" }} />Events</Link></li>
+                <li><Link to="/news"><NewspaperIcon style={{ fill: '#f9fafc', width: "35px", height: "35px", marginRight: "25px" }} />News</Link></li>
+                <li><Link to="/settings"><SettingsIcon style={{ fill: '#f9fafc', width: "35px", height: "35px", marginRight: "25px" }} />Settings</Link></li>
+                <li><Link to="/logout"><LogoutIcon style={{ fill: '#f9fafc', width: "35px", height: "35px", marginRight: "25px" }} />Logout</Link></li>
+            </ul>
         </>
     );
 }
