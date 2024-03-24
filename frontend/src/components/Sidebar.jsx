224c7fce97634159eac1aa01d47f33a2745318fb
Georgios Drivas <78447254+GeorgiosDrivas@users.noreply.github.com>
2024-03-24 18:16:24 +0200

    Update Sidebar.jsx

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,16 +1,18 @@
-export default function Sidebar(){
-    return(
+import { Link } from 'react-router-dom';
+
+export default function Sidebar() {
+    return (
         <>
-        <section className="right-sidebar position-relative w-100">
-            <ul id="sidebar-ul" className="position-fixed">
-                <li>Dashboard</li>
-                <li>Semester</li>
-                <li>Events</li>
-                <li>News</li>
-                <li>Settings</li>
-                <li>Logout</li>
-            </ul>
-        </section>
+            <section className="right-sidebar position-relative w-100">
+                <ul id="sidebar-ul" className="position-fixed">
+                    <li><Link to="/">Dashboard</Link></li>
+                    <li><Link to="/semester">Semester</Link></li>
+                    <li><Link to="/events">Events</Link></li>
+                    <li><Link to="/news">News</Link></li>
+                    <li><Link to="/settings">Settings</Link></li>
+                    <li><Link to="/logout">Logout</Link></li>
+                </ul>
+            </section>
         </>
-    )
+    );
 }
