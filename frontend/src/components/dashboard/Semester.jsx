cf9716b24c7a31bf8ee696bc04c42d7aae90aa56
GiorgosDrivas <drivasgiorgos9@gmail.com>
2024-04-10 00:50:25 +0300

    Css changes in dashboard

--- frontend/src/components/dashboard/Semester.jsx
+++ frontend/src/components/dashboard/Semester.jsx
@@ -4,34 +4,27 @@ export default function Semester({ user }) {
             <div className="row">
                 <div className="col-12">
                     <div className="summary-title mb-5">
-                        <h2>Semester</h2>
+                        <h2>Current Semester</h2>
                     </div>
                 </div>
             </div>
             <div className="row">
-                <div className="col-12">
-                    <div className="accordion" id="accordionExample">
-                        {
-                            (user.courses && user.courses.length > 0) ?
-                                (
-                                    user.courses.map(course => (
-                                        <div className="accordion-item" key={course._id}>
-                                            <h2 className="accordion-header" id={'heading' + course._id}>
-                                                <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target={'#collapse' + course._id} aria-expanded="true" aria-controls={'collapse' + course._id}>
-                                                    <p key={course._id}>{course.course_name}</p>
-                                                </button>
-                                            </h2>
-                                            <div id={'collapse' + course._id} className={`accordion-collapse collapse ${course._id === '65d7b1bfcc7bd8cbea93026f' ? 'show' : ''}`} aria-labelledby={'heading' + course._id} data-bs-parent="#accordionExample">
-                                                <div className="accordion-body">
-                                                    <p key={course._id}>{course.course_teacher}</p>
-                                                </div>
-                                            </div>
+                {
+                    (user.courses && user.courses.length > 0) ?
+                        (
+                            user.courses.map(course => (
+                                <div className="col-12 col-lg-6" key={course._id}>
+                                    <div id="dashboard_single_course" className="mb-5 accordion-item">
+                                        <h2 className="text-center">{course.course_name}</h2>
+                                        <div className="text-center">
+                                            <p>Taught by: {course.course_teacher}</p>
+                                            <p>Grade: <span className="fw-bold grade-span">{course.midterm_grade}</span> out of 100</p>
                                         </div>
-                                    ))
-                                ) : null
-                        }
-                    </div>
-                </div>
+                                    </div>
+                                </div>
+                            ))
+                        ) : null
+                }
             </div>
         </>
     )
