5bd95cefb6171e5d7bfb14dc609cd153439dd171
GiorgosDrivas <drivasgiorgos9@gmail.com>
2024-04-19 13:09:56 +0300

    Change Link to NavLink for future css

--- client/src/components/single-pages/Sidebar.jsx
+++ client/src/components/single-pages/Sidebar.jsx
@@ -1,22 +1,21 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import DashboardIcon from '@mui/icons-material/Dashboard';
 import AutoStoriesIcon from '@mui/icons-material/AutoStories';
 import EventIcon from '@mui/icons-material/Event';
 import NewspaperIcon from '@mui/icons-material/Newspaper';
 import SettingsIcon from '@mui/icons-material/Settings';
 import HomeWorkIcon from '@mui/icons-material/HomeWork';
-import LogoutIcon from '@mui/icons-material/Logout';
 
 export default function Sidebar() {
     return (
         <>
             <ul id="sidebar-ul" className="right-sidebar position-fixed">
-                <li><Link to="/dashboard"><DashboardIcon style={{ fill: '#f9fafc', width: "35px", height: "35px", marginRight: "25px" }} />Dashboard</Link></li>
-                <li><Link to="/semester"><AutoStoriesIcon style={{ fill: '#f9fafc', width: "35px", height: "35px", marginRight: "25px" }} />Semester</Link></li>
-                <li><Link to="/events"><EventIcon style={{ fill: '#f9fafc', width: "35px", height: "35px", marginRight: "25px" }} />Events</Link></li>
-                <li><Link to="/news"><NewspaperIcon style={{ fill: '#f9fafc', width: "35px", height: "35px", marginRight: "25px" }} />News</Link></li>
-                <li><Link to="/exercises"><HomeWorkIcon style={{ fill: '#f9fafc', width: "35px", height: "35px", marginRight: "25px" }} />Exercises</Link></li>
-                <li><Link to="/settings"><SettingsIcon style={{ fill: '#f9fafc', width: "35px", height: "35px", marginRight: "25px" }} />Settings</Link></li>
+                <li><NavLink to="/dashboard"><DashboardIcon style={{ fill: '#f9fafc', width: "35px", height: "35px", marginRight: "25px" }} />Dashboard</NavLink></li>
+                <li><NavLink to="/semester"><AutoStoriesIcon style={{ fill: '#f9fafc', width: "35px", height: "35px", marginRight: "25px" }} />Semester</NavLink></li>
+                <li><NavLink to="/events"><EventIcon style={{ fill: '#f9fafc', width: "35px", height: "35px", marginRight: "25px" }} />Events</NavLink></li>
+                <li><NavLink to="/news"><NewspaperIcon style={{ fill: '#f9fafc', width: "35px", height: "35px", marginRight: "25px" }} />News</NavLink></li>
+                <li><NavLink to="/exercises"><HomeWorkIcon style={{ fill: '#f9fafc', width: "35px", height: "35px", marginRight: "25px" }} />Exercises</NavLink></li>
+                <li><NavLink to="/settings"><SettingsIcon style={{ fill: '#f9fafc', width: "35px", height: "35px", marginRight: "25px" }} />Settings</NavLink></li>
             </ul>
         </>
     );
