b8ea46c94a445fc75992222a17b1bf55229edf13
GiorgosDrivas <drivasgiorgos9@gmail.com>
2024-04-20 16:25:57 +0300

    Create component for failed routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,8 @@ import SemesterSb from './components/single-pages/SemesterPage';
 import Exercises from './components/single-pages/exercises/Exercises';
 import Events from './components/single-pages/EventsPage';
 import Settings from './components/single-pages/SettingsPage';
-import Login from './Login';
+import Login from './components/Login';
+import NotFound from './components/NotFound';
 
 function App() {
 
@@ -22,25 +23,30 @@ function App() {
   }, []);
 
   return (
-    <div className="row">
-      {isLoggedIn && (
-        <div className='col-12 col-lg-1 p-0'>
-          <Sidebar />
+    <>
+      <div className="row">
+        {isLoggedIn && (
+          <div className='col-12 col-lg-1 p-0'>
+            <Sidebar />
+          </div>
+        )}
+        <div className='col-12 col-lg-11 p-0'>
+          <Routes>
+            <Route path="/" element={!isLoggedIn ? <Navigate to="/login" /> : <Navigate to="/dashboard" />} />
+            <Route path="login" element={<Login setIsLoggedIn={setIsLoggedIn} />} />
+            <Route path="dashboard" element={isLoggedIn ? <Dashboard setIsLoggedIn={setIsLoggedIn} /> : <Navigate to="/login" />} />
+            <Route path="semester" element={isLoggedIn ? <SemesterSb /> : <Navigate to="/login" />} />
+            <Route path="events" element={isLoggedIn ? <Events /> : <Navigate to="/login" />} />
+            <Route path="news" element={isLoggedIn ? <News /> : <Navigate to="/login" />} />
+            <Route path="exercises" element={isLoggedIn ? <Exercises /> : <Navigate to="/login" />} />
+            <Route path="settings" element={isLoggedIn ? <Settings setIsLoggedIn={setIsLoggedIn} /> : <Navigate to="/login" />} />
+          </Routes>
         </div>
-      )}
-      <div className='col-12 col-lg-11 p-0'>
-        <Routes>
-          <Route path="/" element={!isLoggedIn ? <Navigate to="/login" /> : <Navigate to="/dashboard" />} />
-          <Route path="/login" element={<Login setIsLoggedIn={setIsLoggedIn} />} />
-          <Route path="/dashboard" element={isLoggedIn ? <Dashboard setIsLoggedIn={setIsLoggedIn} /> : <Navigate to="/login" />} />
-          <Route path="/semester" element={isLoggedIn ? <SemesterSb /> : <Navigate to="/login" />} />
-          <Route path="/events" element={isLoggedIn ? <Events /> : <Navigate to="/login" />} />
-          <Route path="/news" element={isLoggedIn ? <News /> : <Navigate to="/login" />} />
-          <Route path="/exercises" element={isLoggedIn ? <Exercises /> : <Navigate to="/login" />} />
-          <Route path="/settings" element={isLoggedIn ? <Settings setIsLoggedIn={setIsLoggedIn} /> : <Navigate to="/login" />} />
-        </Routes>
       </div>
-    </div>
+      <Routes>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ client/src/components/NotFound.jsx
@@ -0,0 +1,13 @@
+export default function NotFound() {
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-12">
+                    <div className="wrap d-flex justify-content-center align-items-center vh-100">
+                        <h1 className="text-center">The page you are looking for, doesn't exist.</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
