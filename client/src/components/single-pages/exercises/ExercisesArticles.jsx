244ed325b315c7f8112f78dc8063395a36827828
GiorgosDrivas <drivasgiorgos9@gmail.com>
2024-04-21 17:12:45 +0300

    CSS styling

--- client/src/components/single-pages/exercises/ExercisesArticles.jsx
+++ client/src/components/single-pages/exercises/ExercisesArticles.jsx
@@ -7,6 +7,7 @@ export default function ExercisesArticles({ data, exercise }) {
 
     function handleClick() {
         setShowArticle(!showArticle);
+        window.scrollTo(0, 0);
     }
 
     return (
